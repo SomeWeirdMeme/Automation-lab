61384e9044335bf7b1fcb9471b877844295b9446
SomeWeirdMeme <ant.osuant@gmail.com>
2025-10-24 11:27:26 -0400

    wanted to save just incase but regarding calculate pay i have a question abt what the general point of it when it gets calculated by update.

--- Automaton.java
+++ Automaton.java
@@ -62,6 +62,10 @@ public class Automaton
         state = nextState;
     }
     
+    private int calculateNextState(int left, int right, int center){
+        return (left + center + right) % 2;
+    }
+    
     /**
      * Reset the automaton.
      */
